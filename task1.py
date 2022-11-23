cd2a08f11dc388df714eee900ccbb88c8bc27b63
DaryaDectereva <63370331+Darya-tech31@users.noreply.github.com>
2022-11-23 15:31:07 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,38 @@
+import requests
+
+TOKEN = '2619421814940190'  # константа токена
+
+urls_list = [
+    f'https://www.superheroapi.com/api.php/{TOKEN}/search/Hulk',
+    f'https://www.superheroapi.com/api.php/{TOKEN}/search/Thanos',
+    f'https://www.superheroapi.com/api.php/{TOKEN}/search/Captain%America',
+]  # список адресов
+
+def requests_get(url_all):
+    req = (requests.get(url) for url in url_all)
+    return req
+
+def intelligence_req():
+    super_man = []
+    for item in requests_get(urls_list):
+        intelligence = item.json()
+        try:
+            for power_stats in intelligence['results']:
+                super_man.append({
+                    'name': power_stats['name'],
+                    'intelligence': power_stats['powerstats']['intelligence'],
+                })
+        except KeyError:
+            print(f"Проверте ссылки urls: {urls_list}")
+
+    intelligence_super_hero = 0
+    name = ''
+    for intelligence_hero in super_man:
+        if intelligence_super_hero < int(intelligence_hero['intelligence']):
+            intelligence_super_hero = int(intelligence_hero['intelligence'])
+            name = intelligence_hero['name']
+
+    print(f"Самый умный {name}. Его интелект: {intelligence_super_hero}")
+
+
+intelligence_req()
